3ab7f92f66bbb9689fbafc15f52b58d31d183750
Le Tran <lethanhbaotran.letran@gmail.com>
2022-08-07 15:50:51 +0700

    Timer

--- TestTeamProject/Form2.cs
+++ TestTeamProject/Form2.cs
@@ -120,7 +120,7 @@ namespace TestTeamProject
             ///timer Location
 
             label1.Location = new Point(h * 50,0);
-            label2.Location = new Point(h * 50 + 25, 50);
+            label2.Location = new Point(h * 50, 50);
 
             ///create Grid button
 
@@ -145,11 +145,33 @@ namespace TestTeamProject
 
         }
 
-        int time = 0;
+        private string Time(int x)
+        {
+            string s = "";
+            if (x > 9) s = x.ToString();
+            else s = "0" + x.ToString();
+
+            return s;
+        }
+
+        int sec = 0, min = 0, hour = 0;
+
         private void timer1_Tick(object sender, EventArgs e)
         {
-            time++;
-            label2.Text = time.ToString();
+            sec++;
+            if(sec==60)
+            {
+                min++;
+                sec = 0;
+                if (min == 60)
+                {
+                    hour++;
+                    min = 0;
+                }
+            }
+
+            label2.Text = Time(hour) + ":" + Time(min) + ":" + Time(sec);
+
         }
 
         ///Show the number of bombs in each cell
